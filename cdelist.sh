588c579194b90e8d6a78f707db5c2ffcd5c64a11
Marc Chisinevski <marcgcp6@gmail.com>
2022-07-20 22:30:03 -0700

    Create cdelist.sh

---
+++ cdelist.sh
@@ -0,0 +1,3 @@
+/cdelinux  --tls-insecure resource list  --cdp-endpoint https://console-cdp.apps.ecs3.clouderalab.local
+ --vcluster-endpoint  https://9dsd6b7z.cde-zndwdw6m.apps.ecs3.clouderalab.local/dex/api/v1 --access-key-id 0145a949-66b0
+-4640-860d-9d3f1a6b4b00 --access-key-secret mvr+4LR89GXgJopiL7BNvyjUIkCdflwQjlc33462uzo= --auth-no-cache
